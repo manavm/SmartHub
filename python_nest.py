b8cc70ed0abc5310b05d998107eb3ef8bbc665ca
Manav <manavm@utexas.edu>
2015-09-04 15:25:16 -0500

    Resolving merge conflicts

--- python_nest.py
+++ python_nest.py
@@ -4,6 +4,7 @@
 # In[1]:
 
 import nest
+from nest import utils as nest_utils
 import requests
 
 
@@ -18,7 +19,16 @@ password = 'Eric12!@'
 napi = nest.Nest(username, password)
 
 
-# In[5]:
+# In[19]:
+
+def fahrenheit_to_celsius(temp):
+    return nest_utils.f_to_c(temp)
+
+def celsius_to_fahrenheit(temp):
+    return nest_utils.c_to_f(temp)
+
+
+# In[29]:
 
 for structure in napi.structures:
     print 'Structure %s' % structure.name
@@ -28,16 +38,33 @@ for structure in napi.structures:
     for device in structure.devices:
         print '        Device: %s' % device.name
         print '            Temp: %0.1f' % device.temperature
+        print '            Humidity : %0.1f%%' % device.humidity
+        print '            Target   : %0.1fC' % device.target
+        print '            battery_level         : %s' % device.battery_level
 
 
-# In[6]:
+# In[20]:
 
-device.temperature
+current_temp = celsius_to_fahrenheit(device.temperature)
+
+
+# In[21]:
+
+current_temp
 
 
 # In[17]:
 
 device.temperature = 24.444
+
+
+# In[23]:
+
+def get_temperature():
+    return celsius_to_fahrenheit(temperature)
+
+def set_temperature(temp):
+    target_temp = temp
 
 
 # In[ ]:
