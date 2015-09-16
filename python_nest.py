e0a57f00c94c6679743af5c80521cc776befd7a6
Manav <manavm@utexas.edu>
2015-09-16 13:33:28 -0500

    Resolving merge conflicts

--- python_nest.py
+++ python_nest.py
@@ -1,78 +1,50 @@
 
 # coding: utf-8
 
-# In[1]:
-
 import nest
 from nest import utils as nest_utils
 import requests
 
+class Nest:
+    def __init__(self, username = None, password = None):
+        self.username = 'ericmaras@gmail.com'
+        self.password = 'Eric12!@'
+        napi = nest.Nest(self.username, self.password)
+        # for structure in napi.structures:
+        #     print 'Structure %s' % structure.name
+        #     print '    Away: %s' % structure.away
+        #     print '    Devices:'
 
-# In[2]:
+        #     for device in structure.devices:
+        #         print '        Device: %s' % device.name
+        #         print '            Temp: %0.1f' % device.temperature
+        #         print '            Humidity : %0.1f%%' % device.humidity
+        #         print '            Target   : %0.1fC' % device.target
+        #         print '            battery_level         : %s' % device.battery_level
+        structure = napi.structures[0]
+        self.device = device
+        self.temperature = self.c_to_f(device.temperature)
+        self.target = device.target
+        self.humidity = device.humidity
+        self.battery_level = device.battery_level
+        
+        
+    def set_target(self, temp):
+        self.device.temperature = self.f_to_c(temp)
+        self.temperature = self.c_to_f(self.device.temperature)
+    
+    def f_to_c(self, temp):
+        return nest_utils.f_to_c(temp)
 
-username = 'ericmaras@gmail.com'
-password = 'Eric12!@'
+    def c_to_f(self, temp):
+        return nest_utils.c_to_f(temp)
 
-
-# In[3]:
-
-napi = nest.Nest(username, password)
-
-
-# In[19]:
-
-def fahrenheit_to_celsius(temp):
-    return nest_utils.f_to_c(temp)
-
-def celsius_to_fahrenheit(temp):
-    return nest_utils.c_to_f(temp)
-
-
-# In[29]:
-
-for structure in napi.structures:
-    print 'Structure %s' % structure.name
-    print '    Away: %s' % structure.away
-    print '    Devices:'
-
-    for device in structure.devices:
-        print '        Device: %s' % device.name
-        print '            Temp: %0.1f' % device.temperature
-        print '            Humidity : %0.1f%%' % device.humidity
-        print '            Target   : %0.1fC' % device.target
-        print '            battery_level         : %s' % device.battery_level
-
-
-# In[20]:
-
-current_temp = celsius_to_fahrenheit(device.temperature)
-
-
-# In[21]:
-
-current_temp
-
-
-# In[17]:
-
-device.temperature = 24.444
-
-
-# In[23]:
-
-def get_temperature():
-    return celsius_to_fahrenheit(temperature)
-
-def set_temperature(temp):
-    target_temp = temp
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
+if __name__ == "__main__":
+    nest = Nest()
+    print 'Number of arguments:', len(sys.argv), 'arguments.'
+    print 'Argument List:', str(sys.argv)
+    if sys.argv[1] == "get":
+        print "The temperature in Eric's house is " + str(nest.temperature)
+    elif sys.argv[1] == "set":
+        nest.set_target(sys.argv[2])
+        print "The new temperature is " + str(nest.temperature)
